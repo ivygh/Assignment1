1387bc6a06b2c3909461c1251375537e06e38ceb
ivygh <ivychun@gmail.com>
2023-09-27 23:26:42 -0400

    Added const port.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const port = process.env.PORT || 5000
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express');
 var app = express();
-app.listen(5000);
+app.listen(port);
 module.exports = app;
 
-console.log('Server running at http://localhost:5000/');
+console.log('Listening at port ' + port);
